ae3c60b82760a6d8cb1de2a939eae12ff7d2a5ae
HiImVanPhu <phu34521@gmail.com>
2022-05-31 08:59:43 +0700

    test camera

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
 import cv2
 import time
 
-# cap = cv2.VideoCapture(r"rtsp://admin:bk123456@192.168.9.135:554/Streaming/Channels/1/")
-cap = cv2.VideoCapture(r"E:\CameraSanslab\28_04_22\2022-05-19\ch01_00000000061000100.mp4")
+IP = '192.168.9.135'
+cap = cv2.VideoCapture(f"rtsp://admin:bk123456@{IP}:554/Streaming/Channels/1/")
+# cap = cv2.VideoCapture(r"E:\CameraSanslab\28_04_22\2022-05-19\ch01_00000000061000100.mp4")
 
 
 # cap.set(cv2.CAP_PROP_POS_FRAMES, 25)
 count = 0
-while True:
-    count += 1
-    ret, frame = cap.read()
-    # frame = cv2.resize(frame, dsize=None, fx=0.5, fy=0.5)
-    cv2.imshow("image", frame)
-    key = cv2.waitKey(100)
-    if key == ord("q"):
-        break
+
+# while True:
+count += 1
+ret, frame = cap.read()
+# frame = cv2.resize(frame, dsize=None, fx=0.5, fy=0.5)
+cv2.imshow("image", frame)
+cv2.waitKey(0)
+# key = cv2.waitKey(1)
+# if key == ord("q"):
+#     break
+
     # if count == 25:
     #     time.sleep(4)
     #     count += 4 * 25
